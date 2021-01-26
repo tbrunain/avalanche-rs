a6062b11e925a9c5129d118b84fad88cfa2c1444
tbrunain <tristan@settlemint.com>
2021-01-26 20:48:54 +0100

    fix: Lint

--- src/pvm/create_subnet_tx.rs
+++ src/pvm/create_subnet_tx.rs
@@ -1,7 +1,7 @@
 use crate::avm::parser::Context;
+use crate::pvm::atomic_block_parser::Transaction;
 use crate::pvm::base_tx_parser::base_tx_parser;
 use crate::pvm::output_parser::{secp256k1_output_owner_output_parser, SECP256KTransferOutput};
-use crate::pvm::atomic_block_parser::Transaction;
 use crate::utils::conversion::pop_i32;
 
 use std::borrow::Borrow;
